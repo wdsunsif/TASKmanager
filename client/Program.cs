2dd486cc85bf2546c12766d490046e9d96de540e
Faig Ibragimov <faigibragimov88@gmail.com>
2024-09-12 18:07:01 +0400

    Добавьте файлы проекта.

---
+++ client/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using client;
+
+var ip = IPAddress.Loopback;
+var port = 4444;
+
+var client = new TcpClient();
+client.Connect(ip, port);
+
+var stream = client.GetStream();
+var br = new BinaryReader(stream);
+var bw = new BinaryWriter(stream);
+Command command = null!;
+string response = null!;
+Console.WriteLine($"client started [{ip}:{port}]");
+while (true)
+{
+    Console.WriteLine("Write command or HELP: ");
+
+    var strtxt = Console.ReadLine()!.ToUpper();
+
+    if (strtxt == "HELP")
+    {
+
+        Console.WriteLine();
+        Console.WriteLine("Command List : ");
+        Console.WriteLine(Command.ProcessList);
+        Console.WriteLine($"{Command.Run} <process_name>");
+        Console.WriteLine($"{Command.Kill} <process_name>");
+        Console.WriteLine("HELP");
+        Console.ReadLine();
+        Console.Clear();
+        continue;
+    }
+
+    var input = strtxt.Split(' ');
+    switch (input[0])
+    {
+        case Command.ProcessList:
+            command = new Command { Text = input[0] };
+            bw.Write(JsonSerializer.Serialize(command));
+            response = br.ReadString();
+            var processList = JsonSerializer.Deserialize<string[]>(response);
+            foreach (var processName in processList!)
+            {
+                Console.WriteLine($"{processName}");
+            }
+            Console.ReadLine();
+            Console.Clear();
+            break;
+        case Command.Run:
+            command = new Command { Text = input[0], Param = input[1] };
+            bw.Write(JsonSerializer.Serialize(command));
+            response = br.ReadString();
+            Console.ReadLine();
+            break;
+
+        case Command.Kill:
+            command = new Command { Text = input[0], Param = input[1] };
+            bw.Write(JsonSerializer.Serialize(command));
+            Console.ReadLine();
+            Console.Clear();
+            break;
+        default:
+            break;
+    }
+
+}
---
+++ server/Program.cs
@@ -0,0 +1,58 @@
+﻿using server;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+var ip = IPAddress.Loopback;
+var port = 4444;
+
+var listener = new TcpListener(ip, port);
+
+listener.Start();
+
+
+Console.WriteLine("Server started");
+Thread.Sleep(1000);
+Console.WriteLine($"Server login from: {ip} | {port}");
+Thread.Sleep(1000);
+Console.WriteLine("Listening...\n");
+
+
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    var stream = client.GetStream();
+    var br = new BinaryReader(stream);
+    var bw = new BinaryWriter(stream);
+
+
+    while (true)
+    {
+        var input = br.ReadString();
+        var command = JsonSerializer.Deserialize<Command>(input);
+
+        if (command is null) continue;
+        Console.WriteLine(command.Text);
+        Console.WriteLine(command.Param);
+        switch (command.Text)
+        {
+            case Command.Run:
+                string processName = command.Param!;
+                Process.Start(processName);
+                Console.WriteLine($"New task run - {DateTime.Now}");
+                break;
+
+            case Command.Kill:
+                processName = command.Param!;
+                Process process = Process.GetProcessesByName(processName)[0];
+                process.Kill();
+                Console.WriteLine($"Any task has kill - {DateTime.Now}");
+                break;
+
+            default:
+                break;
+        }
+    }
+}
